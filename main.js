8b5a8a9f313fd8e443a267c3b2501780a9587942
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2023-06-29 17:38:31 -0300

    madal comprar por whatsapp

--- main.js
+++ main.js
@@ -27,3 +27,157 @@ carrusel.addEventListener("mouseout", () => {
 });
 
 start();
+
+//////////////funcion comprar y efectuar comprar uno
+  function mostrarTabla() {
+    var tabla = document.getElementById("tablaCompra");
+    var boton = document.getElementById("comprarButton");
+    
+    if (tabla.style.display === "none") {
+      tabla.style.display = "block";
+      boton.textContent = "Desativar COMPRAR";
+      boton.style.backgroundColor = "red";
+      boton.style.color = "white";
+    } else {
+      tabla.style.display = "none";
+      boton.textContent = "Ativar COMPRAR";
+      boton.style.backgroundColor = "green";
+      boton.style.color = "white";
+    }
+  }
+
+////////////////funcion comprar e efctuar comprar dos
+
+function mostrarTablados() {
+  var tabla = document.getElementById("tablaComprados");
+  var boton = document.getElementById("comprarButtondos");
+  
+  if (tabla.style.display === "none") {
+    tabla.style.display = "block";
+    boton.textContent = "Desativar COMPRAR";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+  } else {
+    tabla.style.display = "none";
+    boton.textContent = "Ativar COMPRAR";
+    boton.style.backgroundColor = "green";
+    boton.style.color = "white";
+  }
+}
+
+function mostrarTablatres() {
+  var tabla = document.getElementById("tablaCompratres");
+  var boton = document.getElementById("comprarButtontres");
+  
+  if (tabla.style.display === "none") {
+    tabla.style.display = "block";
+    boton.textContent = "Desativar COMPRAR";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+  } else {
+    tabla.style.display = "none";
+    boton.textContent = "Ativar COMPRAR";
+    boton.style.backgroundColor = "green";
+    boton.style.color = "white";
+  }
+}
+
+function mostrarTablaquatro() {
+  var tabla = document.getElementById("tablaCompraquatro");
+  var boton = document.getElementById("comprarButtonquatro");
+  
+  if (tabla.style.display === "none") {
+    tabla.style.display = "block";
+    boton.textContent = "Desativar COMPRAR";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+  } else {
+    tabla.style.display = "none";
+    boton.textContent = "Ativar COMPRAR";
+    boton.style.backgroundColor = "green";
+    boton.style.color = "white";
+  }
+}
+
+function mostrarTablacinco() {
+  var tabla = document.getElementById("tablaCompracinco");
+  var boton = document.getElementById("comprarButtoncinco");
+  
+  if (tabla.style.display === "none") {
+    tabla.style.display = "block";
+    boton.textContent = "Desativar COMPRAR";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+  } else {
+    tabla.style.display = "none";
+    boton.textContent = "Ativar COMPRAR";
+    boton.style.backgroundColor = "green";
+    boton.style.color = "white";
+  }
+}
+
+function mostrarTablaseis() {
+  var tabla = document.getElementById("tablaCompraseis");
+  var boton = document.getElementById("comprarButtonseis");
+  
+  if (tabla.style.display === "none") {
+    tabla.style.display = "block";
+    boton.textContent = "Desativar COMPRAR";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+  } else {
+    tabla.style.display = "none";
+    boton.textContent = "Ativar COMPRAR";
+    boton.style.backgroundColor = "green";
+    boton.style.color = "white";
+  }
+}
+function mostrarTablasiete() {
+  var tabla = document.getElementById("tablaComprasiete");
+  var boton = document.getElementById("comprarButtonsiete");
+  
+  if (tabla.style.display === "none") {
+    tabla.style.display = "block";
+    boton.textContent = "Desativar COMPRAR";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+  } else {
+    tabla.style.display = "none";
+    boton.textContent = "Ativar COMPRAR";
+    boton.style.backgroundColor = "green";
+    boton.style.color = "white";
+  }
+}
+
+function mostrarTablaocho() {
+  var tabla = document.getElementById("tablaCompraocho");
+  var boton = document.getElementById("comprarButtonocho");
+  
+  if (tabla.style.display === "none") {
+    tabla.style.display = "block";
+    boton.textContent = "Desativar COMPRAR";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+  } else {
+    tabla.style.display = "none";
+    boton.textContent = "Ativar COMPRAR";
+    boton.style.backgroundColor = "green";
+    boton.style.color = "white";
+  }
+}
+function mostrarTablanueve() {
+  var tabla = document.getElementById("tablaCompranueve");
+  var boton = document.getElementById("comprarButtonnueve");
+  
+  if (tabla.style.display === "none") {
+    tabla.style.display = "block";
+    boton.textContent = "Desativar COMPRAR";
+    boton.style.backgroundColor = "red";
+    boton.style.color = "white";
+  } else {
+    tabla.style.display = "none";
+    boton.textContent = "Ativar COMPRAR";
+    boton.style.backgroundColor = "green";
+    boton.style.color = "white";
+  }
+}
